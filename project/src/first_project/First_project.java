e2f4e4d7f46e0eec0707c18be59644063cbb31ce
Comrade <57000785+asmfahim@users.noreply.github.com>
2022-05-30 22:31:24 +0600

    Data type

--- project/src/first_project/First_project.java
+++ project/src/first_project/First_project.java
@@ -3,5 +3,19 @@ package first_project;
 public class First_project {
     public static void main(String[] args){
         System.out.println(" Our BuBt");
+
+        boolean b = true;
+        int i = 2147483647 ;
+        short s = 32767 ;
+        char c = 'a';
+        float f = 32.2f;
+        double d = 35.1235;
+        System.out.println(b);
+        System.out.println(i);
+        System.out.println(c);
+        System.out.println(s);
+        System.out.println(f);
+        System.out.println(d);
+
     }
 }
