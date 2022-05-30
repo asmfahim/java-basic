3bde3c3102b31d8d44de0a05711b2d95d608213b
Comrade <57000785+asmfahim@users.noreply.github.com>
2022-05-30 23:20:48 +0600

    Update for understand Assignment Operator

--- project/src/first_project/AssignmentOp.java
+++ project/src/first_project/AssignmentOp.java
@@ -3,26 +3,22 @@ import java.util.*;
 
 public class AssignmentOp {
     public static void main(String[] args) {
-        int x,y,z,v,w;
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Two Numbers : ");
         int a = sc.nextInt();
         int b = sc.nextInt();
 
-        x=a+b;
-        System.out.println("Assignment Operator '=' x=a+b Add : " + x );
         a+=b;
         System.out.println("Assignment Operator '+=' a+=b Add : " + a );
-        b-=x;
-        System.out.println("Assignment Operator '-=' b-=x Sub : " + b );
-        y=b;
-        y*=a;
-        System.out.println("Assignment Operator '*=' y=b,y*=a Multi : " + y );
-        z=a;
-        z/=b;
-        System.out.println("Assignment Operator '/=' z=a,z/=b divide : " + z );
-        x%=z;
-        System.out.println("Assignment Operator '%=' x%=z Modulus : " + x );
+        a-=b;
+        System.out.println("Assignment Operator '-=' a-=b Sub : " + a );
+        a*=b;
+        System.out.println("Assignment Operator '*=' a*=b Multi : " + a );
+        a/=b;
+        System.out.println("Assignment Operator '/=' a/=b divide : " + a );
+        a%=b;
+        System.out.println("Assignment Operator '%=' a%=b Modulus : " + a );
 
     }
 }
