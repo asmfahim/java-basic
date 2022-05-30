0226639e1f5f23ccf3472362b1697fd2387e8747
Comrade <57000785+asmfahim@users.noreply.github.com>
2022-05-30 22:48:47 +0600

    ArithmeticDemo

---
+++ project/src/first_project/ArithmeticProb.java
@@ -0,0 +1,20 @@
+package first_project;
+import java.util.*;
+
+public class ArithmeticProb {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Two Numbers :");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("Addition : " + (a+b));
+        System.out.println("Subtraction : " + (a-b));
+        System.out.println("Multiplication : " + (a*b));
+        System.out.println("Division : " + (a/b));
+        System.out.println("Modulus : " + (a%b));
+
+    }
+}
