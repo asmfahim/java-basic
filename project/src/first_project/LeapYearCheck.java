1ef3876a977f03f1058482186ab771b855453672
Comrade <57000785+asmfahim@users.noreply.github.com>
2022-06-02 03:12:00 +0600

    Leap Year Check

---
+++ project/src/first_project/LeapYearCheck.java
@@ -0,0 +1,17 @@
+package first_project;
+
+import java.util.Scanner;
+
+public class LeapYearCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year;
+        System.out.println("Enter a Year : ");
+        year = sc.nextInt();
+        if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)){
+            System.out.println(year + " is a Leap Year.");
+        }else {
+            System.out.println(year + " is not a Leap Year.");
+        }
+    }
+}
